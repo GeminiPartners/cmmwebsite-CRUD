58a2789815f561e37d0c1a3660e23f414207a137
Brian Furches <dbfurches@gmail.com>
2019-08-17 12:45:15 -0400

    market autn query working

--- models/authModel.js
+++ models/authModel.js
@@ -1,8 +1,12 @@
 const knex = require('./connection');
 
 module.exports = {    
-  ItemtypeAuthQuery: function (id) 
-    return knex('itemtype')
-        .where({'id' : id}).;
+  MarketItemTypeAuth: function (user_id) {
+    console.log('about to return infor for user ', user_id)
+    return knex('market')
+    .join('user_market', {'market.id': 'user_market.market_id'})
+    .leftOuterJoin('itemtype', {'user_market.market_id': 'itemtype.itemtypemarket'})
+    .select('itemtype.id', 'itemtype.itemtypename', 'user_market.market_id', 'user_market.user_id', 'user_market.role')
+    .where({'user_market.user_id' : user_id});
   }
 }
---
+++ test/unit/models/auth_test.js
@@ -0,0 +1,26 @@
+const Auth = require('../../../models/authModel');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const chaiInteger = require('chai-integer');
+
+// set up the middleware
+chai.use(chaiAsPromised);
+chai.use(chaiInteger);
+const should = require('chai').should() 
+
+
+
+describe('Unit: auth.MarketItemTypeAuth', function() {
+  context('With valid id', function() {
+    const expectedResult = "Eyepieces";
+    const user_id = 2
+    it('should return an itemtype', function() {
+      return Auth
+        .MarketItemTypeAuth(user_id)
+        .then(results => {
+          console.log('market auth results: ', results)
+          results[0].itemtypename.should.equal(expectedResult);
+        });    
+    });  
+  });
+});
